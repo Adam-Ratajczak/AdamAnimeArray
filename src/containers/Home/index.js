3f2078f7971bee4291f6db8c169636e59dd49f9e
Adam Ratajczak <a3.ratajczak@gmail.com>
2022-12-16 18:10:10 +0100

    Menubar

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
+import {Menubar} from '../../widgets'
 
 class Home extends Component {
   render() {
     return (
-      <div className="not-found">
-        <h1>200</h1>
-        <h3>Ok Home</h3>
+      <div className="content">
+        <Menubar/>
       </div>
     )
   }
--- src/widgets/Menubar/index.js
+++ src/widgets/Menubar/index.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react'
+import './style.scss';
 
-class AnimeInfo extends Component {
+class Menubar extends Component {
   render() {
     return (
-      <div className="not-found">
-        <h1>200</h1>
-        <h3>Ok</h3>
+      <div id="content">
+        <div id="HeaderContent">
+          <h1 id="aaaHeader">AdamAnimeArray</h1>
+          <form id="Search">
+            <input type="submit" id="Searchbtn" value=""></input>
+            <input type="text" id="Searchbar" placeholder="Search..."></input>
+          </form>
+        </div>
+        <div id="MenuContent">
+          <div class="Button" id="anime_top_ranked"><p>Top Ranked</p></div>
+          <div class="Button" id="anime_newest"><p>Newest</p></div>
+          <div class="Button" id="anime_recomended"><p>Recomended</p></div>
+          <div class="Button" id="anime_random"><p>Random</p></div>
+        </div>
       </div>
     )
   }
 }
 
-export default AnimeInfo
+export default Menubar
