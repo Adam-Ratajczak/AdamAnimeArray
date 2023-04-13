168a316eb2fad2394e16071e7493565b2d125ca2
Maciej <sppmacd@pm.me>
2023-04-13 22:32:20 +0200

    Add loading icon to anime lists

--- src/widgets/AnimePoster/index.js
+++ src/widgets/AnimePoster/index.js
@@ -1,79 +1,98 @@
-import React, {useEffect, useState} from 'react'
-import { GetAnime, GetEpisodes, GetAnimeType } from '../../db_module'
-import './style.scss';
+import React, { useEffect, useState } from "react";
+import { GetAnime, GetEpisodes, GetAnimeType } from "../../db_module";
+import "./style.scss";
 
-function AnimePoster(props){
-  const [AnimeTitle, SetAnimeTitle] = useState(0)
-  const [AnimePoster, SetAnimePoster] = useState(0)
-  const [Premiered, SetPremiered] = useState(0)
-  const [EpisodeNum, SetEpisodeNum] = useState(0)
-  const [AnimeType, SetAnimeType] = useState(0)
-  
-  useEffect(() =>{
+function AnimePoster(props) {
+  const [AnimeTitle, SetAnimeTitle] = useState(null);
+  const [AnimePoster, SetAnimePoster] = useState(null);
+  const [Premiered, SetPremiered] = useState(null);
+  const [EpisodeNum, SetEpisodeNum] = useState(null);
+  const [AnimeType, SetAnimeType] = useState(null);
+
+  function isLoaded() {
+    return (
+      AnimeTitle !== null &&
+      AnimePoster !== null &&
+      Premiered !== null &&
+      EpisodeNum !== null &&
+      AnimeType !== null
+    );
+  }
+
+  useEffect(() => {
     GetAnime(props.AnimeID)
-    .then((response) => response.json())
-    .then((result) =>{
-      SetAnimeTitle(result.AnimeTitle);
-      SetAnimePoster(result.PosterURL);
-      SetPremiered(result.Premiered);
-    })
+      .then((response) => response.json())
+      .then((result) => {
+        SetAnimeTitle(result.AnimeTitle);
+        SetAnimePoster(result.PosterURL);
+        SetPremiered(result.Premiered);
+      });
   }, []);
-  
-  useEffect(() =>{
+
+  useEffect(() => {
     GetEpisodes(props.AnimeID)
-    .then((response) => response.json())
-    .then((result) =>{
-      SetEpisodeNum(result.length);
-    })
+      .then((response) => response.json())
+      .then((result) => {
+        SetEpisodeNum(result.length);
+      });
   }, []);
 
-  useEffect(() =>{
+  useEffect(() => {
     GetAnimeType(props.AnimeID)
-    .then((response) => response.json())
-    .then((result) =>{
-      SetAnimeType(result.Name)
-    })
+      .then((response) => response.json())
+      .then((result) => {
+        SetAnimeType(result.Name);
+      });
   }, []);
 
-  let short_title = "";
-
-  if(AnimeTitle.toString().length > 15){
-    short_title = AnimeTitle.toString().substring(0, 12) + "...";
-  }else{
-    short_title = AnimeTitle;
+  function shortTitle() {
+    if (!AnimeTitle) {
+      return "Loading...";
+    }
+    if (AnimeTitle.toString().length > 15) {
+      return AnimeTitle.toString().substring(0, 12) + "...";
+    } else {
+      return AnimeTitle;
+    }
   }
 
   return (
-    <a href={'/anime/' + props.AnimeID.toString()}>
-    <div class="AnimePoster">
-      <div class="AnimePosterHover">
-          <div class="AnimePosterHeader tooltip">
-          <span class="tooltiptext">{AnimeTitle}</span>
-            <h1>{short_title}</h1>
-          </div>
-          <div class="AnimePosterDiv">
-            <img class="AnimePosterImg" src={AnimePoster.toString()}></img>
-          </div>
-          <table class="AnimePosterInfo">
-            <tbody>
-              <tr>
-                <td class="PropertyName">Type: </td>
-                <td>{(AnimeType) ? AnimeType.toString() : "Unknown"}</td>
-              </tr>
-              <tr>
-                <td class="PropertyName">Premiered: </td>
-                <td>{(Premiered) ? Premiered.toString() : "Unknown"}</td>
-              </tr>
-              <tr>
-                <td class="PropertyName">Episodes:</td>
-                <td>{(EpisodeNum) ? EpisodeNum.toString() : "Unknown"}</td>
-              </tr>
-            </tbody>
-          </table>
+    <a href={"/anime/" + props.AnimeID.toString()}>
+      <div class="AnimePoster">
+        <div class="AnimePosterHover">
+          {isLoaded() ? (
+            <>
+              <div class="AnimePosterHeader tooltip">
+                <span class="tooltiptext">{AnimeTitle}</span>
+                <h1>{shortTitle()}</h1>
+              </div>
+              <div class="AnimePosterDiv">
+                <img class="AnimePosterImg" src={AnimePoster.toString()}></img>
+              </div>
+              <table class="AnimePosterInfo">
+                <tbody>
+                  <tr>
+                    <td class="PropertyName">Type: </td>
+                    <td>{AnimeType ? AnimeType.toString() : "Unknown"}</td>
+                  </tr>
+                  <tr>
+                    <td class="PropertyName">Premiered: </td>
+                    <td>{Premiered ? Premiered.toString() : "Unknown"}</td>
+                  </tr>
+                  <tr>
+                    <td class="PropertyName">Episodes:</td>
+                    <td>{EpisodeNum ? EpisodeNum.toString() : "Unknown"}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </>
+          ) : (
+            <div class="Loading"></div>
+          )}
+        </div>
       </div>
-    </div>
     </a>
-  )
+  );
 }
 
-export default AnimePoster
+export default AnimePoster;
