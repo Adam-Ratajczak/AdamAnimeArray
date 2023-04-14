83bb29e9ba81b290b04d779e16adda3d4d71a76e
Maciej <sppmacd@pm.me>
2023-04-14 20:35:02 +0200

    Revert "Work on loading and error handling on homepage"
    This reverts commit 95fd4d2002883a49770b52121fd4e3f37d4106b4.
    
    It actually broke paging on homepage and caused duplicate entries,
    somehow.

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,101 +1,65 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { Menubar, AnimePoster, TabUnit } from "../../widgets";
-import { GetAnime } from "../../db_module";
-import redirect from "../../redirect";
-import "./style.scss";
+import React, { useCallback, useEffect, useState } from 'react'
+import { Menubar, AnimePoster, TabUnit } from '../../widgets'
+import { GetAnime } from "../../db_module"
+import redirect from '../../redirect'
+import './style.scss';
 
 const num_sample_animes = Math.floor(window.screen.width / 270) * 3;
 
 function Home() {
-  const [AnimeContainer, SetAnimeContainer] = useState(null);
-  const [AnimeTabUnit, SetAnimeTabUnit] = useState(0);
-  const [AnimeCountBegin, SetAnimeCountBegin] = useState(0);
-
-  function isLoaded() {
-    return AnimeContainer !== null;
-  }
+  const [AnimeHeader, SetAnimeHeader] = useState("")
+  const [AnimeContainer, SetAnimeContainer] = useState([])
+  const [AnimeTabUnit, SetAnimeTabUnit] = useState(0)
+  const [AnimeCountBegin, SetAnimeCountBegin] = useState(0)
 
   const siteType = window.location.href.split("/").at(-1);
-
-  function getAnimeHeader() {
-    let anime_header = "";
-    switch (siteType) {
-      case "Popular":
-        anime_header = "Top Ranked Animes";
-        break;
-      case "Recomended":
-        anime_header = "Recommended Animes";
-        break;
-      case "Newest":
-        anime_header = "Recently Added Animes";
-        break;
-      case "Random":
-        anime_header = "Picking a random anime...";
-        break;
-      default:
-        break;
-    }
-    return anime_header;
-  }
-
   const fillList = useCallback(() => {
     GetAnime()
       .then((response) => response.json())
       .then((result) => {
-        // Hacky error handling btw here
-        if (!(result instanceof Array)) {
-          SetAnimeContainer(
-            <div style={{ color: "white", marginBottom: "15px" }}>
-              {result.message}
-            </div>
-          );
-          return;
-        }
-
-        SetAnimeContainer([]);
-        let AnimeCountEnd = Math.min(
-          AnimeCountBegin + num_sample_animes,
-          result.length
-        );
+        SetAnimeContainer([])
+        let AnimeCountEnd = Math.min(AnimeCountBegin + num_sample_animes, result.length);
 
         let anime_id_arr = [];
+        let anime_header = ""
 
-        if (siteType === "Random") {
-          let num = Math.floor(
-            (Math.random() * (result.length + 1)) % result.length
-          );
+        if (siteType == "Random") {
+          let num = Math.floor((Math.random() * (result.length + 1)) % result.length);
           redirect("Anime/" + num);
-        } else if (siteType === "Popular") {
+        } else if (siteType == "Popular") {
+          anime_header = "Top Ranked Animes";
+
           for (let i = AnimeCountBegin; i < AnimeCountEnd; i++) {
             anime_id_arr.push(result[i].AnimeID);
           }
-        } else if (siteType === "Recomended") {
+        } else if (siteType == "Recomended") {
+          anime_header = "Recomended Animes";
+
           for (let i = AnimeCountBegin; i < AnimeCountEnd; i++) {
-            let AnimeNum = Math.floor(
-              (Math.random() * (result.length + 1)) % result.length
-            );
+            let AnimeNum = Math.floor((Math.random() * (result.length + 1)) % result.length);
             anime_id_arr.push(result[AnimeNum].AnimeID);
           }
-        } else if (siteType === "Newest") {
+        } else if (siteType == "Newest") {
+          anime_header = "Recently Added Animes";
           let to_sort = result;
           to_sort.sort((a, b) => {
             if (!a.AiredBegin.Valid) {
               return 1;
             }
 
-            if (a.AiredBegin.Time < "1900-01-01T00:00:00Z") {
-              return -1;
+            if(a.AiredBegin.Time < "1900-01-01T00:00:00Z"){
+              return -1
             }
 
-            const ABegin = new Date(a.AiredBegin.Time);
-            const BBegin = new Date(b.AiredBegin.Time);
+            const ABegin = new Date(a.AiredBegin.Time)
+            const BBegin = new Date(b.AiredBegin.Time)
 
-            if (ABegin < BBegin) {
-              return 1;
-            } else if (ABegin === BBegin) {
-              return 0;
-            } else {
-              return -1;
+            if(ABegin < BBegin){
+              return 1
+            }else if(ABegin == BBegin){
+              return 0
+            }else{
+              return -1
             }
           });
 
@@ -108,46 +72,37 @@ function Home() {
 
         for (let id of anime_id_arr) {
           const AnimeID = id.toString();
-          container.push(<AnimePoster AnimeID={AnimeID} key={AnimeID} />);
+          container.push((<AnimePoster AnimeID={AnimeID} key={AnimeID}/>))
         }
 
         const foo = (event, index) => {
           SetAnimeCountBegin((index - 1) * num_sample_animes);
-          SetAnimeContainer([]);
-        };
+          SetAnimeContainer([])
+        }
 
-        SetAnimeContainer(container);
-        SetAnimeTabUnit(
-          <TabUnit
-            TabCount={Math.ceil(result.length / num_sample_animes)}
-            TabCollapse="10"
-            onChange={foo}
-          />
-        );
-      });
-  }, [AnimeCountBegin, siteType]);
+        SetAnimeHeader(anime_header)
+        SetAnimeContainer(container)
+        SetAnimeTabUnit((<TabUnit TabCount={Math.ceil(result.length / num_sample_animes)} TabCollapse="10" onChange={foo} />))
+      })
+  })
 
-  if (AnimeContainer === null) {
-    fillList();
+  if(AnimeContainer.length == 0){
+    fillList()
   }
   return (
     <div id="main">
       <Menubar />
       <div id="content">
         <div id="ContentHeader">
-          <h1>{getAnimeHeader()}</h1>
+          <h1>{AnimeHeader}</h1>
         </div>
         <div id="SampleAnimeList">
-          {isLoaded() ? (
-            AnimeContainer
-          ) : (
-            <div class="Loading" style={{ marginBottom: "20px" }} />
-          )}
+          {AnimeContainer}
         </div>
       </div>
       {AnimeTabUnit}
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
