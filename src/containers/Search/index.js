928f66f5e9780a0b3896ec58ff692274e54f029c
Adam Ratajczak <a3.ratajczak@gmail.com>
2023-04-15 09:28:53 +0200

    More responsive search & duplicates in result bugfix

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -3,7 +3,7 @@ import { AnimePanel, Menubar } from '../../widgets'
 import { FilterAnimes, GetDemographics, GetGenre, GetProducer, GetTheme, GetType } from '../../db_module';
 import './style.scss'
 
-const animeLimit = 100
+const animeLimit = 50
 
 class TypeButton extends Component {
   render() {
@@ -12,7 +12,7 @@ class TypeButton extends Component {
     return (
       <div class="CoolButtonDiv">
         <input type="checkbox" class="CoolButtonCb" id={cb_id} name={cb_id} onChange={this.props.onChange} value={this.props.TypeID} />
-        <label class="CoolButton" for={cb_id}>
+        <label class="CoolButton unselectable" for={cb_id}>
           {this.props.AnimeName}
         </label>
       </div>
@@ -21,9 +21,9 @@ class TypeButton extends Component {
 }
 
 class Search extends Component {
-  constructor(){
+  constructor() {
     super();
-  
+
     this.state = {
       Genres: [],
       Themes: [],
@@ -32,7 +32,7 @@ class Search extends Component {
       Demographics: [],
       Types: [],
       Animes: [],
-  
+
       GenreList: [],
       ThemeList: [],
       StudioList: [],
@@ -40,269 +40,309 @@ class Search extends Component {
       DemographicsList: [],
       TypeList: []
     }
-  
+
     this.GetButtons = this.GetButtons.bind(this);
   }
-  
-  GetButtons(typename, result) {
-      result.sort((a, b) => {
-        return a.Name > b.Name;
-      });
-  
-      let res = [];
-      for (let elem of result) {
-        function getArr(input_arr, input_elem) {
-          let arr = input_arr;
-  
-          if (arr.indexOf(input_elem) == -1) {
-            arr.push(input_elem);
-          } else {
-            arr = arr.filter((value, index, array) => {
-              return value != input_elem;
-            })
-          }
-  
-          return arr;
-        }
-  
-        let callback;
-        if (typename == "genre") {
-          callback = () => {
-            const arr = getArr(this.state.GenreList, elem.ID);
-  
-            let newState = this.state;
-            newState.GenreList = arr;
-            this.setState(newState);
-          };
-        } else if (typename == "theme") {
-          callback = () => {
-            const arr = getArr(this.state.ThemeList, elem.ID);
-  
-            let newState = this.state;
-            newState.ThemeList = arr;
-            this.setState(newState);
-          };
-        } else if (typename == "studio") {
-          callback = () => {
-            const arr = getArr(this.state.StudioList, elem.ID);
-  
-            let newState = this.state;
-            newState.ProducerList = arr;
-            this.setState(newState);
-          };
-        } else if (typename == "producer") {
-          callback = () => {
-            const arr = getArr(this.state.ProducerList, elem.ID);
-  
-            let newState = this.state;
-            newState.ProducerList = arr;
-            this.setState(newState);
-          };
-        } else if (typename == "demographics") {
-          callback = () => {
-            const arr = getArr(this.state.DemographicsList, elem.ID);
-  
-            let newState = this.state;
-            newState.DemographicsList = arr;
-            this.setState(newState);
-          };
-        } else if (typename == "type") {
-          callback = () => {
-            const arr = getArr(this.state.TypeList, elem.ID);
-  
-            let newState = this.state;
-            newState.TypeList = arr;
-            this.setState(newState);
-          };
-        }
-        res.push((<TypeButton TypeName={typename} TypeID={elem.ID} AnimeName={elem.Name == "" ? "Unknown" : elem.Name.toString()} onChange={callback} />))
-      }
-  
-      return res;
-    }
-  
-  componentDidMount(){
-    GetGenre()
-      .then((response) => response.json())
-      .then((result) => {
-        const res = this.GetButtons("genre", result);
-  
-        let newState = this.state;
-        newState.Genres = res;
-        this.setState(newState);
-      });
-  
-    GetTheme()
-      .then((response) => response.json())
-      .then((result) => {
-        const res = this.GetButtons("theme", result);
-  
-        let newState = this.state;
-        newState.Themes = res;
-        this.setState(newState);
-      });
-  
-    GetProducer()
-      .then((response) => response.json())
-      .then((result) => {
-        const res = this.GetButtons("studio", result);
-    
-        let newState = this.state;
-        newState.Studios = res;
-        this.setState(newState);
-    });
-  
-    GetProducer()
-      .then((response) => response.json())
-      .then((result) => {
-        const res = this.GetButtons("producer", result);
-  
-        let newState = this.state;
-        newState.Producers = res;
-        this.setState(newState);
-      });
-  
-    GetDemographics()
-      .then((response) => response.json())
-      .then((result) => {
-        const res = this.GetButtons("demographics", result);
-  
-        let newState = this.state;
-        newState.Demographics = res;
-        this.setState(newState);
-      });
-  
-    GetType()
-      .then((response) => response.json())
-      .then((result) => {
-        const res = this.GetButtons("type", result);
-  
-        let newState = this.state;
-        newState.Types = res;
-        this.setState(newState);
-      });
 
-  const SearchFoo = (start, len) => {
+  SearchFoo(start, len) {
     const AnimeName = document.getElementById("SearchbarLarge").value;
 
     FilterAnimes(AnimeName, this.state.TypeList, this.state.GenreList, this.state.ThemeList, this.state.StudioList, this.state.ProducerList, this.state.DemographicsList)
       .then((response) => response.json())
       .then((result) => {
-          let res = this.state.Animes;
+        let res = this.state.Animes;
 
-          for(let i = start; i < Math.min(start + len, result.length); i++){
+        for (let i = start; i < Math.min(start + len, result.length); i++) {
+          if (res.indexOf(result[i]) == -1) {
             res.push(result[i]);
           }
+        }
+
+        let newState = this.state;
+        newState.Animes = res;
+        this.setState(newState);
+      })
+  }
+
+  GetButtons(typename, result) {
+    result.sort((a, b) => {
+      return a.Name > b.Name;
+    });
+
+    let res = [];
+    for (let elem of result) {
+      function getArr(input_arr, input_elem) {
+        let arr = input_arr;
+
+        if (arr.indexOf(input_elem) == -1) {
+          arr.push(input_elem);
+        } else {
+          arr = arr.filter((value, index, array) => {
+            return value != input_elem;
+          })
+        }
+
+        return arr;
+      }
+
+      let callback;
+      if (typename == "genre") {
+        callback = () => {
+          const arr = getArr(this.state.GenreList, elem.ID);
 
           let newState = this.state;
-          newState.Animes = res;
+          newState.GenreList = arr;
+          newState.Animes = [];
           this.setState(newState);
-        })
-  }
+          this.SearchFoo(0, animeLimit)
+        };
+      } else if (typename == "theme") {
+        callback = () => {
+          const arr = getArr(this.state.ThemeList, elem.ID);
 
-  let searchPhraze = window.location.href.split("/").at(-1).split("%20").join(" ");
+          let newState = this.state;
+          newState.ThemeList = arr;
+          newState.Animes = [];
+          this.setState(newState);
+          this.SearchFoo(0, animeLimit)
+        };
+      } else if (typename == "studio") {
+        callback = () => {
+          const arr = getArr(this.state.StudioList, elem.ID);
 
-  if(searchPhraze == "*"){
-    searchPhraze = "";
-  }
+          let newState = this.state;
+          newState.ProducerList = arr;
+          newState.Animes = [];
+          this.setState(newState);
+          this.SearchFoo(0, animeLimit)
+        };
+      } else if (typename == "producer") {
+        callback = () => {
+          const arr = getArr(this.state.ProducerList, elem.ID);
 
-  document.getElementById("SearchbarLarge").value = searchPhraze;
+          let newState = this.state;
+          newState.ProducerList = arr;
+          newState.Animes = [];
+          this.setState(newState);
+          this.SearchFoo(0, animeLimit)
+        };
+      } else if (typename == "demographics") {
+        callback = () => {
+          const arr = getArr(this.state.DemographicsList, elem.ID);
 
-  SearchFoo(0, animeLimit);
+          let newState = this.state;
+          newState.DemographicsList = arr;
+          newState.Animes = [];
+          this.setState(newState);
+          this.SearchFoo(0, animeLimit)
+        };
+      } else if (typename == "type") {
+        callback = () => {
+          const arr = getArr(this.state.TypeList, elem.ID);
 
-  document.getElementById("SearchbtnLarge").onclick = () => {
-    let newState = this.state;
-    newState.Animes = [];
-    this.setState(newState);
-
-    SearchFoo(0, animeLimit);
-  }
-
-  document.getElementById("SearchBarDiv").onsubmit = (event) => {
-    let newState = this.state;
-    newState.Animes = [];
-    this.setState(newState);
-
-    SearchFoo(0, animeLimit);
-
-    event.preventDefault();
-  }
-  
-  document.onscroll = () => {
-    let documentHeight = document.body.scrollHeight;
-    let currentScroll = window.scrollY + window.innerHeight;
-    
-    let modifier = 0; 
-    if(currentScroll + modifier > documentHeight) {
-      SearchFoo(this.state.Animes.length, animeLimit);
+          let newState = this.state;
+          newState.TypeList = arr;
+          newState.Animes = [];
+          this.setState(newState);
+          this.SearchFoo(0, animeLimit)
+        };
+      }
+      res.push((<TypeButton TypeName={typename} TypeID={elem.ID} AnimeName={elem.Name == "" ? "Unknown" : elem.Name.toString()} onChange={callback} />))
     }
+
+    return res;
   }
 
-  let headers = document.querySelectorAll(".GenreHeader")
-  let arrows = document.querySelectorAll(".arrow")
-  let btns = document.querySelectorAll(".AnimeTypeBtnDiv")
+  componentDidMount() {
+    GetGenre()
+      .then((response) => response.json())
+      .then((result) => {
+        const res = this.GetButtons("genre", result);
 
-  for(let i = 0; i < headers.length; i++){
-    headers[i].onclick = () =>{
-      if(arrows[i].classList.contains("right")){
-        arrows[i].classList.remove("right")
-        arrows[i].classList.add("down")
-        btns[i].style.display = "block"
-      }else if(arrows[i].classList.contains("down")){
-        arrows[i].classList.remove("down")
-        arrows[i].classList.add("right")
-        btns[i].style.display = "none"
+        let newState = this.state;
+        newState.Genres = res;
+        this.setState(newState);
+      });
+
+    GetTheme()
+      .then((response) => response.json())
+      .then((result) => {
+        const res = this.GetButtons("theme", result);
+
+        let newState = this.state;
+        newState.Themes = res;
+        this.setState(newState);
+      });
+
+    GetProducer()
+      .then((response) => response.json())
+      .then((result) => {
+        const res = this.GetButtons("studio", result);
+
+        let newState = this.state;
+        newState.Studios = res;
+        this.setState(newState);
+      });
+
+    GetProducer()
+      .then((response) => response.json())
+      .then((result) => {
+        const res = this.GetButtons("producer", result);
+
+        let newState = this.state;
+        newState.Producers = res;
+        this.setState(newState);
+      });
+
+    GetDemographics()
+      .then((response) => response.json())
+      .then((result) => {
+        const res = this.GetButtons("demographics", result);
+
+        let newState = this.state;
+        newState.Demographics = res;
+        this.setState(newState);
+      });
+
+    GetType()
+      .then((response) => response.json())
+      .then((result) => {
+        const res = this.GetButtons("type", result);
+
+        let newState = this.state;
+        newState.Types = res;
+        this.setState(newState);
+      });
+
+    let searchPhraze = window.location.href.split("/").at(-1).split("%20").join(" ");
+
+    if (searchPhraze == "*") {
+      searchPhraze = "";
+    }
+
+    document.getElementById("SearchbarLarge").value = searchPhraze;
+
+    this.SearchFoo(0, animeLimit);
+
+    document.getElementById("SearchbtnLarge").onclick = () => {
+      document.getElementById("SearchbarLarge").value = "";
+
+      let cblist = document.querySelectorAll(".CoolButtonCb")
+
+      for (let cb of cblist) {
+        cb.checked = false
+      }
+
+      let newState = this.state;
+      newState.GenreList = [];
+      newState.ThemeList = [];
+      newState.ProducerList = [];
+      newState.StudioList = [];
+      newState.DemographicsList = [];
+      newState.TypeList = [];
+
+      this.setState(newState);
+
+      this.SearchFoo(0, animeLimit);
+    }
+
+    document.getElementById("SearchBarDiv").onsubmit = (event) => {
+      let newState = this.state;
+      newState.Animes = [];
+      this.setState(newState);
+
+      this.SearchFoo(0, animeLimit);
+
+      event.preventDefault();
+    }
+
+    document.getElementById("SearchbarLarge").onkeydown = (event) => {
+      if (document.getElementById("SearchbarLarge").value.length < 2) {
+        return
+      }
+
+      let newState = this.state;
+      newState.Animes = [];
+      this.setState(newState);
+
+      this.SearchFoo(0, animeLimit);
+    }
+
+    document.onscroll = () => {
+      let documentHeight = document.body.scrollHeight;
+      let currentScroll = window.scrollY + window.innerHeight;
+
+      let modifier = 0;
+      if (currentScroll + modifier > documentHeight) {
+        this.SearchFoo(this.state.Animes.length, animeLimit);
+      }
+    }
+
+    let headers = document.querySelectorAll(".GenreHeader")
+    let arrows = document.querySelectorAll(".arrow")
+    let btns = document.querySelectorAll(".AnimeTypeBtnDiv")
+
+    for (let i = 0; i < headers.length; i++) {
+      headers[i].onclick = () => {
+        if (arrows[i].classList.contains("right")) {
+          arrows[i].classList.remove("right")
+          arrows[i].classList.add("down")
+          btns[i].style.display = "block"
+        } else if (arrows[i].classList.contains("down")) {
+          arrows[i].classList.remove("down")
+          arrows[i].classList.add("right")
+          btns[i].style.display = "none"
+        }
       }
     }
   }
-}
 
-render(){
-  return (
-    <div id="main">
-      <Menubar />
-      <div id="contentSearch">
-        <div id="SearchDiv">
-          <form id="SearchBarDiv">
-            <input type="text" id="SearchbarLarge" placeholder="Search..."></input>
-            <input type="button" id="SearchbtnLarge" value="Find"></input>
-          </form>
-          <div id="SearchResults">
-            {this.state.Animes.map(anime => {
-              return (<AnimePanel AnimeID={anime.AnimeID} />)
-            })}
+  render() {
+    return (
+      <div id="main">
+        <Menubar />
+        <div id="contentSearch">
+          <div id="SearchDiv">
+            <form id="SearchBarDiv">
+              <input type="text" id="SearchbarLarge" placeholder="Search..." />
+              <input type="button" id="SearchbtnLarge" value="Clear" />
+            </form>
+            <div id="SearchResults">
+              {this.state.Animes.map(anime => {
+                return (<AnimePanel AnimeID={anime.AnimeID} />)
+              })}
+            </div>
           </div>
-        </div>
-        <div id="AnimeTypeDiv">
-          <h3 class="GenreHeader">Genres<i class="arrow right"></i></h3>
-          <div class="AnimeTypeBtnDiv">
-            {this.state.Genres}
-          </div>
-          <h3 class="GenreHeader">Themes<i class="arrow right"></i></h3>
-          <div class="AnimeTypeBtnDiv">
-            {this.state.Themes}
-          </div>
-          <h3 class="GenreHeader">Demographics<i class="arrow right"></i></h3>
-          <div class="AnimeTypeBtnDiv">
-            {this.state.Demographics}
-          </div>
-          <h3 class="GenreHeader">Types<i class="arrow right"></i></h3>
-          <div class="AnimeTypeBtnDiv">
-            {this.state.Types}
-          </div>
-          <h3 class="GenreHeader">Studios<i class="arrow right"></i></h3>
-          <div class="AnimeTypeBtnDiv">
-            {this.state.Studios}
-          </div>
-          <h3 class="GenreHeader">Producers<i class="arrow right"></i></h3>
-          <div class="AnimeTypeBtnDiv">
-            {this.state.Producers}
+          <div id="AnimeTypeDiv">
+            <h3 class="GenreHeader">Genres<i class="arrow right"></i></h3>
+            <div class="AnimeTypeBtnDiv">
+              {this.state.Genres}
+            </div>
+            <h3 class="GenreHeader">Themes<i class="arrow right"></i></h3>
+            <div class="AnimeTypeBtnDiv">
+              {this.state.Themes}
+            </div>
+            <h3 class="GenreHeader">Demographics<i class="arrow right"></i></h3>
+            <div class="AnimeTypeBtnDiv">
+              {this.state.Demographics}
+            </div>
+            <h3 class="GenreHeader">Types<i class="arrow right"></i></h3>
+            <div class="AnimeTypeBtnDiv">
+              {this.state.Types}
+            </div>
+            <h3 class="GenreHeader">Studios<i class="arrow right"></i></h3>
+            <div class="AnimeTypeBtnDiv">
+              {this.state.Studios}
+            </div>
+            <h3 class="GenreHeader">Producers<i class="arrow right"></i></h3>
+            <div class="AnimeTypeBtnDiv">
+              {this.state.Producers}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
 }
 
--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {Home, NotFound, AnimeInfo, AnimePlayer, Search, Login, Signup, NoPlayer} from './containers'
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { Home, NotFound, AnimeInfo, AnimePlayer, Search, Login, Signup, NoPlayer } from './containers'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/Recomended" element={<Home />}></Route>
-          <Route path="*/Recomended" element={<Navigate to="/Recomended"/>}></Route>
-          <Route path="/Popular" element={<Home />}></Route>
-          <Route path="*/Popular" element={<Navigate to="/Popular"/>}></Route>
-          <Route path="/Newest" element={<Home />}></Route>
-          <Route path="*/Newest" element={<Navigate to="/Newest"/>}></Route>
-          <Route path="/Login" element={<Login />}></Route>
-          <Route path="/Signup" element={<Signup />}></Route>
-          <Route path='/' element={<Navigate to="/Recomended"/>}></Route>
-          <Route path="/Anime/:AnimeID" element={<AnimeInfo />}></Route>
-          <Route path="/Anime/:AnimeID/Ep/:EpID" element={<AnimePlayer />}></Route>
-          <Route path="/Anime/:AnimeID/Ep" element={<Navigate to={"/Anime/" + window.location.href.split("/").at(-2)} />}></Route>
-          <Route path='/Anime' element={<Navigate to="/Search"/>}></Route>
-          <Route path="/Search/:searchrule" element={<Search />}></Route>
-          <Route path='/Search' element={<Navigate to="/Search/*"/>}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-          <Route path="/NoPlayer" element={<NoPlayer />}></Route>
+        <Route path="/Recomended" element={<Home />}></Route>
+        <Route path="*/Recomended" element={<Navigate to="/Recomended" />}></Route>
+        <Route path="/Popular" element={<Home />}></Route>
+        <Route path="*/Popular" element={<Navigate to="/Popular" />}></Route>
+        <Route path="/Newest" element={<Home />}></Route>
+        <Route path="*/Newest" element={<Navigate to="/Newest" />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Signup" element={<Signup />}></Route>
+        <Route path='/' element={<Navigate to="/Recomended" />}></Route>
+        <Route path="/Anime/:AnimeID" element={<AnimeInfo />}></Route>
+        <Route path="/Anime/:AnimeID/Ep/:EpID" element={<AnimePlayer />}></Route>
+        <Route path="/Anime/:AnimeID/Ep" element={<Navigate to={"/Anime/" + window.location.href.split("/").at(-2)} />}></Route>
+        <Route path='/Anime' element={<Navigate to="/Search" />}></Route>
+        <Route path="/Search/:searchrule" element={<Search />}></Route>
+        <Route path='/Search' element={<Navigate to="/Search/*" />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+        <Route path="/NoPlayer" element={<NoPlayer />}></Route>
       </Routes>
     </BrowserRouter>
   );
