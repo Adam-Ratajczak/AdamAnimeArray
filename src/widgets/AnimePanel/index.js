4c2471911b1045ff6e323424828c4857ab94e501
Adam Ratajczak <a3.ratajczak@gmail.com>
2023-05-19 08:00:17 +0200

    Get rid of UseState in AnimePanel

--- src/widgets/AnimePanel/index.js
+++ src/widgets/AnimePanel/index.js
@@ -3,17 +3,6 @@ import './style.scss';
 import { applyEffect } from 'fluent-reveal-effect';
 
 function AnimePanel(props) {
-  const [AnimeTitle, SetAnimeTitle] = useState(0)
-  const [AnimePoster, SetAnimePoster] = useState(0)
-  const [Premiered, SetPremiered] = useState(0)
-  const [EpisodeNum, SetEpisodeNum] = useState(0)
-  const [AnimeType, SetAnimeType] = useState(0)
-  const [AnimeGenres, SetAnimeGenres] = useState(0)
-  const [AnimeThemes, SetAnimeThemes] = useState(0)
-  const [AnimeStudios, SetAnimeStudios] = useState(0)
-  const [AnimeProducers, SetAnimeProducers] = useState(0)
-  const [AnimeDemographics, SetAnimeDemographics] = useState(0)
-
   function get_genre_str(arr) {
     let res = "";
 
@@ -29,17 +18,6 @@ function AnimePanel(props) {
   }
 
   useEffect(() => {
-    SetAnimeTitle(props.Anime.AnimeTitle);
-    SetAnimePoster(props.Anime.PosterURL);
-    SetPremiered(props.Anime.Premiered);
-    SetEpisodeNum(props.Anime.EpisodeNum);
-    SetAnimeGenres(get_genre_str(props.Anime.Genres));
-    SetAnimeThemes(get_genre_str(props.Anime.Themes));
-    SetAnimeStudios(get_genre_str(props.Anime.Studios));
-    SetAnimeProducers(get_genre_str(props.Anime.Producers));
-    SetAnimeDemographics(get_genre_str(props.Anime.Demographics));
-    SetAnimeType(props.Anime.Type.Name)
-
     applyEffect('.AnimePanelHover', {
       // clickEffect: true,
       lightColor: 'rgba(255,255,255,0.6)',
@@ -57,25 +35,25 @@ function AnimePanel(props) {
   return (
     <a href={'/anime/' + props.Anime.AnimeID.toString()}>
       <div class="AnimePanel">
-        <img class="AnimePanelImg" src={AnimePoster}></img>
+        <img class="AnimePanelImg" src={props.Anime.PosterURL}></img>
         <div class="AnimePanelHover">
           <div class="AnimePanelHeader">
-            <h1>{AnimeTitle}</h1>
+            <h1>{props.Anime.AnimeTitle}</h1>
           </div>
           <div class="AnimePanelContent btn">
             <table class="AnimePanelInfo">
               <tbody>
                 <tr>
                   <td class="PropertyName">Type: </td>
-                  <td>{(AnimeType) ? AnimeType.toString() : "Unknown"}</td>
+                  <td>{(props.Anime.Type.Name) ? props.Anime.Type.Name.toString() : "Unknown"}</td>
                 </tr>
                 <tr>
                   <td class="PropertyName">Premiered: </td>
-                  <td>{(Premiered) ? Premiered.toString() : "Unknown"}</td>
+                  <td>{(props.Anime.Premiered) ? props.Anime.Premiered.toString() : "Unknown"}</td>
                 </tr>
                 <tr>
                   <td class="PropertyName">Episodes:</td>
-                  <td>{(EpisodeNum) ? EpisodeNum.toString() : "Unknown"}</td>
+                  <td>{(props.Anime.EpisodeNum) ? props.Anime.EpisodeNum.toString() : "Unknown"}</td>
                 </tr>
               </tbody>
             </table>
@@ -83,23 +61,23 @@ function AnimePanel(props) {
               <tbody>
                 <tr>
                   <td class="PropertyName">Genres: </td>
-                  <td>{(AnimeGenres) ? AnimeGenres : "None"}</td>
+                  <td>{(props.Anime.Genres.length != 0) ? get_genre_str(props.Anime.Genres) : "None"}</td>
                 </tr>
                 <tr>
                   <td class="PropertyName">Themes: </td>
-                  <td>{(AnimeThemes) ? AnimeThemes : "None"}</td>
+                  <td>{(props.Anime.Themes.length != 0) ? get_genre_str(props.Anime.Themes) : "None"}</td>
                 </tr>
                 <tr>
                   <td class="PropertyName">Studios:</td>
-                  <td>{(AnimeStudios) ? AnimeStudios : "None"}</td>
+                  <td>{(props.Anime.Studios.length != 0) ? get_genre_str(props.Anime.Studios) : "None"}</td>
                 </tr>
                 <tr>
                   <td class="PropertyName">Producers:</td>
-                  <td>{(AnimeProducers) ? AnimeProducers : "None"}</td>
+                  <td>{(props.Anime.Producers.length != 0) ? get_genre_str(props.Anime.Producers) : "None"}</td>
                 </tr>
                 <tr>
                   <td class="PropertyName">Demographics:</td>
-                  <td>{(AnimeDemographics) ? AnimeDemographics : "None"}</td>
+                  <td>{(props.Anime.Demographics.length != 0) ? get_genre_str(props.Anime.Demographics) : "None"}</td>
                 </tr>
               </tbody>
             </table>
